acec76f08d5b7f570e63c7001eac485488b49a82
Kyuusha <daaddimaki04@gmail.com>
2025-12-20 20:53:12 +0700

    setup auth with laravel  breeze

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,7 +44,9 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-
+        
+        // Merubah route nya
         return redirect(route('dashboard', absolute: false));
     }
 }
+
